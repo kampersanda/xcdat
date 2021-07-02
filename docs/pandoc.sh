2aeee082d436ae90002322e5794fb8b037d18123
Shunsuke Kanda <shnsk.knd@gmail.com>
2021-07-02 21:55:13 +0900

    rm

--- docs/pandoc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-pandoc --template=template.html -o index.html document.md -c style.css --toc --toc-depth=2
--- include/xcdat.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef XCDAT_XCDAT_HPP_
-#define XCDAT_XCDAT_HPP_
-
-#include "xcdat/TrieBuilder.hpp"
-
-#endif //XCDAT_XCDAT_HPP_
